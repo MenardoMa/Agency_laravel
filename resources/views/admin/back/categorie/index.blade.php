816e656b463f9e272363f9fd9095c9b825693ed8
Menardo Madiumba <menardotera@gmail.com>
2026-04-10 12:09:30 +0100

    Fixe component categorie, Add, table and btn create

--- resources/views/admin/back/categorie/index.blade.php
+++ resources/views/admin/back/categorie/index.blade.php
@@ -4,4 +4,38 @@
 
 @section('content')
     <h3>Cetegorie</h3>
+    <div class="my-3 d-flex align-items-center justify-content-between">
+        <p>Liste</p>
+        <a href="" class="btn btn-primary btn-sm">Create categorie</a>
+    </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
